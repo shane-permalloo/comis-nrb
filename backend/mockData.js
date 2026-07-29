7156a269d173cb251828db0b153abd388278189a
shane-permalloo <shane.permalloo@mns.mu>
2026-07-29 12:01:40 +0400

    Add more users to the mockdata

--- backend/mockData.js
+++ backend/mockData.js
@@ -46,5 +46,138 @@ module.exports = [
     maritalStatus: 'Single', birthDistrict: 'BLANTYRE CITY', address: 'BLANTYRE CITY~LIMBE~LIMBE RESIDENTIAL AREA',
     dateofbirth: '20/August/1995', dateofIssue: '01/September/2015', dateofExpiry: '01/September/2035',
     phoneNumber: '0992233445', fingerposition: '4', status: 'SEE NRB'
+  },
+  // --- Additional VALID members for QA testing ---
+  {
+    idno: 'VLD00101', firstname: 'CHISOMO', othernames: '', surname: 'BANDA', gender: 'Female',
+    maritalStatus: 'Single', birthDistrict: 'LILONGWE CITY', address: 'LILONGWE CITY~AREA 43~AREA 43 RESIDENTIAL AREA',
+    dateofbirth: '05/May/1993', dateofIssue: '10/June/2013', dateofExpiry: '01/January/2080',
+    phoneNumber: '0881234001', fingerposition: '1', status: 'VALID'
+  },
+  {
+    idno: 'VLD00102', firstname: 'KONDWANI', othernames: '', surname: 'PHIRI', gender: 'Male',
+    maritalStatus: 'Single', birthDistrict: 'BLANTYRE CITY', address: 'BLANTYRE CITY~CHIRIMBA~CHIRIMBA RESIDENTIAL AREA',
+    dateofbirth: '12/March/1988', dateofIssue: '20/April/2008', dateofExpiry: '01/January/2080',
+    phoneNumber: '0992345002', fingerposition: '3', status: 'VALID'
+  },
+  {
+    idno: 'VLD00103', firstname: 'MPHATSO', othernames: 'JAMES', surname: 'MWALE', gender: 'Male',
+    maritalStatus: 'Married', birthDistrict: 'MZUZU CITY', address: 'MZUZU CITY~CHIBAVI~CHIBAVI RESIDENTIAL AREA',
+    dateofbirth: '22/July/1985', dateofIssue: '01/August/2005', dateofExpiry: '01/January/2080',
+    phoneNumber: '0881234003', fingerposition: '2', status: 'VALID'
+  },
+  {
+    idno: 'VLD00104', firstname: 'GRACE', othernames: '', surname: 'TEMBO', gender: 'Female',
+    maritalStatus: 'Single', birthDistrict: 'ZOMBA CITY', address: 'ZOMBA CITY~SADZI~SADZI RESIDENTIAL AREA',
+    dateofbirth: '17/November/1995', dateofIssue: '01/December/2015', dateofExpiry: '01/January/2080',
+    phoneNumber: '0992345004', fingerposition: '4', status: 'VALID'
+  },
+  {
+    idno: 'VLD00105', firstname: 'INNOCENT', othernames: '', surname: 'CHIRWA', gender: 'Male',
+    maritalStatus: 'Single', birthDistrict: 'LILONGWE CITY', address: 'LILONGWE CITY~AREA 36~AREA 36 RESIDENTIAL AREA',
+    dateofbirth: '03/February/1991', dateofIssue: '15/March/2011', dateofExpiry: '01/January/2080',
+    phoneNumber: '0881234005', fingerposition: '1', status: 'VALID'
+  },
+  {
+    idno: 'VLD00106', firstname: 'ESTHER', othernames: 'RUTH', surname: 'KAMANGA', gender: 'Female',
+    maritalStatus: 'Married', birthDistrict: 'BLANTYRE CITY', address: 'BLANTYRE CITY~BANGWE~BANGWE RESIDENTIAL AREA',
+    dateofbirth: '28/September/1987', dateofIssue: '10/October/2007', dateofExpiry: '01/January/2080',
+    phoneNumber: '0992345006', fingerposition: '2', status: 'VALID'
+  },
+  {
+    idno: 'VLD00107', firstname: 'DALITSO', othernames: '', surname: 'NKHOMA', gender: 'Male',
+    maritalStatus: 'Single', birthDistrict: 'MZUZU CITY', address: 'MZUZU CITY~MZIMU~MZIMU RESIDENTIAL AREA',
+    dateofbirth: '14/April/1994', dateofIssue: '01/May/2014', dateofExpiry: '01/January/2080',
+    phoneNumber: '0881234007', fingerposition: '3', status: 'VALID'
+  },
+  {
+    idno: 'VLD00108', firstname: 'TAKONDWA', othernames: '', surname: 'GONDWE', gender: 'Female',
+    maritalStatus: 'Single', birthDistrict: 'LILONGWE CITY', address: 'LILONGWE CITY~AREA 49~AREA 49 RESIDENTIAL AREA',
+    dateofbirth: '09/January/1996', dateofIssue: '20/February/2016', dateofExpiry: '01/January/2080',
+    phoneNumber: '0992345008', fingerposition: '1', status: 'VALID'
+  },
+  {
+    idno: 'VLD00109', firstname: 'FELIX', othernames: 'PAUL', surname: 'MSISKA', gender: 'Male',
+    maritalStatus: 'Married', birthDistrict: 'BLANTYRE CITY', address: 'BLANTYRE CITY~NDIRANDE~NDIRANDE RESIDENTIAL AREA',
+    dateofbirth: '25/June/1983', dateofIssue: '01/July/2003', dateofExpiry: '01/January/2080',
+    phoneNumber: '0881234009', fingerposition: '5', status: 'VALID'
+  },
+  {
+    idno: 'VLD00110', firstname: 'STELLA', othernames: '', surname: 'NYIRENDA', gender: 'Female',
+    maritalStatus: 'Single', birthDistrict: 'ZOMBA CITY', address: 'ZOMBA CITY~MATAWALE~MATAWALE RESIDENTIAL AREA',
+    dateofbirth: '30/October/1990', dateofIssue: '15/November/2010', dateofExpiry: '01/January/2080',
+    phoneNumber: '0992345010', fingerposition: '2', status: 'VALID'
+  },
+  {
+    idno: 'VLD00111', firstname: 'AUBREY', othernames: '', surname: 'JERE', gender: 'Male',
+    maritalStatus: 'Single', birthDistrict: 'LILONGWE CITY', address: 'LILONGWE CITY~AREA 25~AREA 25 RESIDENTIAL AREA',
+    dateofbirth: '07/August/1992', dateofIssue: '20/September/2012', dateofExpiry: '01/January/2080',
+    phoneNumber: '0881234011', fingerposition: '4', status: 'VALID'
+  },
+  {
+    idno: 'VLD00112', firstname: 'PATRICIA', othernames: 'ANN', surname: 'KAUNDA', gender: 'Female',
+    maritalStatus: 'Married', birthDistrict: 'BLANTYRE CITY', address: 'BLANTYRE CITY~LIMBE~LIMBE RESIDENTIAL AREA',
+    dateofbirth: '19/December/1986', dateofIssue: '01/January/2007', dateofExpiry: '01/January/2080',
+    phoneNumber: '0992345012', fingerposition: '3', status: 'VALID'
+  },
+  {
+    idno: 'VLD00113', firstname: 'WYCLIFFE', othernames: '', surname: 'CHAVULA', gender: 'Male',
+    maritalStatus: 'Single', birthDistrict: 'MZUZU CITY', address: 'MZUZU CITY~KATOTO~KATOTO RESIDENTIAL AREA',
+    dateofbirth: '11/February/1997', dateofIssue: '25/March/2017', dateofExpiry: '01/January/2080',
+    phoneNumber: '0881234013', fingerposition: '1', status: 'VALID'
+  },
+  {
+    idno: 'VLD00114', firstname: 'ANNIE', othernames: '', surname: 'MKANDAWIRE', gender: 'Female',
+    maritalStatus: 'Single', birthDistrict: 'LILONGWE CITY', address: 'LILONGWE CITY~AREA 18~AREA 18 RESIDENTIAL AREA',
+    dateofbirth: '26/April/1994', dateofIssue: '10/May/2014', dateofExpiry: '01/January/2080',
+    phoneNumber: '0992345014', fingerposition: '2', status: 'VALID'
+  },
+  {
+    idno: 'VLD00115', firstname: 'OLIVER', othernames: 'JOHN', surname: 'MUNTHALI', gender: 'Male',
+    maritalStatus: 'Married', birthDistrict: 'BLANTYRE CITY', address: 'BLANTYRE CITY~CHIRIMBA~CHIRIMBA RESIDENTIAL AREA',
+    dateofbirth: '02/September/1984', dateofIssue: '15/October/2004', dateofExpiry: '01/January/2080',
+    phoneNumber: '0881234015', fingerposition: '6', status: 'VALID'
+  },
+  {
+    idno: 'VLD00116', firstname: 'BERNADETTE', othernames: '', surname: 'ZUZE', gender: 'Female',
+    maritalStatus: 'Single', birthDistrict: 'ZOMBA CITY', address: 'ZOMBA CITY~SADZI~SADZI RESIDENTIAL AREA',
+    dateofbirth: '16/July/1993', dateofIssue: '01/August/2013', dateofExpiry: '01/January/2080',
+    phoneNumber: '0992345016', fingerposition: '4', status: 'VALID'
+  },
+  {
+    idno: 'VLD00117', firstname: 'HASTINGS', othernames: '', surname: 'KAPITO', gender: 'Male',
+    maritalStatus: 'Single', birthDistrict: 'LILONGWE CITY', address: 'LILONGWE CITY~AREA 36~AREA 36 RESIDENTIAL AREA',
+    dateofbirth: '23/March/1989', dateofIssue: '10/April/2009', dateofExpiry: '01/January/2080',
+    phoneNumber: '0881234017', fingerposition: '3', status: 'VALID'
+  },
+  {
+    idno: 'VLD00118', firstname: 'MERCY', othernames: 'HOPE', surname: 'NGWIRA', gender: 'Female',
+    maritalStatus: 'Married', birthDistrict: 'MZUZU CITY', address: 'MZUZU CITY~CHIBAVI~CHIBAVI RESIDENTIAL AREA',
+    dateofbirth: '08/November/1991', dateofIssue: '20/December/2011', dateofExpiry: '01/January/2080',
+    phoneNumber: '0992345018', fingerposition: '2', status: 'VALID'
+  },
+  {
+    idno: 'VLD00119', firstname: 'RAPHAEL', othernames: '', surname: 'KACHINGWE', gender: 'Male',
+    maritalStatus: 'Single', birthDistrict: 'BLANTYRE CITY', address: 'BLANTYRE CITY~BANGWE~BANGWE RESIDENTIAL AREA',
+    dateofbirth: '15/January/1998', dateofIssue: '01/February/2018', dateofExpiry: '01/January/2080',
+    phoneNumber: '0881234019', fingerposition: '1', status: 'VALID'
+  },
+  {
+    idno: 'VLD00120', firstname: 'DIANA', othernames: '', surname: 'MALOPA', gender: 'Female',
+    maritalStatus: 'Single', birthDistrict: 'LILONGWE CITY', address: 'LILONGWE CITY~AREA 49~AREA 49 RESIDENTIAL AREA',
+    dateofbirth: '31/May/1995', dateofIssue: '15/June/2015', dateofExpiry: '01/January/2080',
+    phoneNumber: '0992345020', fingerposition: '5', status: 'VALID'
+  },
+  {
+    idno: 'VLD00121', firstname: 'WINSTON', othernames: 'CHARLES', surname: 'KADZAMIRA', gender: 'Male',
+    maritalStatus: 'Married', birthDistrict: 'ZOMBA CITY', address: 'ZOMBA CITY~MATAWALE~MATAWALE RESIDENTIAL AREA',
+    dateofbirth: '19/October/1982', dateofIssue: '05/November/2002', dateofExpiry: '01/January/2080',
+    phoneNumber: '0881234021', fingerposition: '4', status: 'VALID'
+  },
+  {
+    idno: 'VLD00122', firstname: 'NATASHA', othernames: '', surname: 'THOLE', gender: 'Female',
+    maritalStatus: 'Single', birthDistrict: 'MZUZU CITY', address: 'MZUZU CITY~MZIMU~MZIMU RESIDENTIAL AREA',
+    dateofbirth: '04/August/1997', dateofIssue: '20/September/2017', dateofExpiry: '01/January/2080',
+    phoneNumber: '0992345022', fingerposition: '3', status: 'VALID'
   }
 ];
--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -77,14 +77,38 @@ export class App {
   maxDob: Date;
 
   testData = [
-    { idno: 'MA845876', firstname: 'LOVE', othernames: '', surname: 'GRACE', gender: 'Female', dob: '01/January/1990', expiry: '01/January/2080', status: 'VALID' },
-    { idno: 'AA55DDFF', firstname: 'BRUCE', othernames: '', surname: 'PROMISE', gender: 'Male', dob: '15/June/1990', expiry: '01/January/2080', status: 'VALID' },
-    { idno: 'MINOR001', firstname: 'TOM', othernames: '', surname: 'YOUNG', gender: 'Male', dob: '01/January/2012', expiry: '01/January/2080', status: 'VALID (Minor)' },
-    { idno: 'EXP00148', firstname: 'JANE', othernames: '', surname: 'OLD', gender: 'Female', dob: '01/May/1988', expiry: '01/January/2020', status: 'EXPIRED' },
-    { idno: 'DEC00199', firstname: 'BLACKNEZ', othernames: 'AGO', surname: 'POPE', gender: 'Male', dob: '01/January/1900', expiry: '01/January/1930', status: 'PERSON DECEASED' },
-    { idno: 'INV00122', firstname: 'MARK', othernames: '', surname: 'BAD', gender: 'Male', dob: '01/January/1985', expiry: '01/January/2030', status: 'INVALID' },
-    { idno: 'REN00233', firstname: 'SARAH', othernames: '', surname: 'RENEWED', gender: 'Female', dob: '10/March/1992', expiry: '01/January/2022', status: 'RENEWAL PROCESSED' },
-    { idno: 'SNR00344', firstname: 'PETER', othernames: '', surname: 'FLAGGED', gender: 'Male', dob: '20/August/1995', expiry: '01/September/2035', status: 'SEE NRB' }
+    // Original entries (mixed statuses for edge-case testing)
+    { idno: 'MA845876', firstname: 'LOVE',       othernames: '',     surname: 'GRACE',       gender: 'Female', dob: '01/January/1990',  expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'AA55DDFF', firstname: 'BRUCE',      othernames: '',     surname: 'PROMISE',     gender: 'Male',   dob: '15/June/1990',     expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'MINOR001', firstname: 'TOM',        othernames: '',     surname: 'YOUNG',       gender: 'Male',   dob: '01/January/2012',  expiry: '01/January/2080',  status: 'VALID (Minor)' },
+    { idno: 'EXP00148', firstname: 'JANE',       othernames: '',     surname: 'OLD',         gender: 'Female', dob: '01/May/1988',      expiry: '01/January/2020',  status: 'EXPIRED' },
+    { idno: 'DEC00199', firstname: 'BLACKNEZ',   othernames: 'AGO',  surname: 'POPE',        gender: 'Male',   dob: '01/January/1900',  expiry: '01/January/1930',  status: 'PERSON DECEASED' },
+    { idno: 'INV00122', firstname: 'MARK',       othernames: '',     surname: 'BAD',         gender: 'Male',   dob: '01/January/1985',  expiry: '01/January/2030',  status: 'INVALID' },
+    { idno: 'REN00233', firstname: 'SARAH',      othernames: '',     surname: 'RENEWED',     gender: 'Female', dob: '10/March/1992',    expiry: '01/January/2022',  status: 'RENEWAL PROCESSED' },
+    { idno: 'SNR00344', firstname: 'PETER',      othernames: '',     surname: 'FLAGGED',     gender: 'Male',   dob: '20/August/1995',   expiry: '01/September/2035', status: 'SEE NRB' },
+    // Additional VALID members
+    { idno: 'VLD00101', firstname: 'CHISOMO',    othernames: '',        surname: 'BANDA',       gender: 'Female', dob: '05/May/1993',      expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00102', firstname: 'KONDWANI',   othernames: '',        surname: 'PHIRI',       gender: 'Male',   dob: '12/March/1988',    expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00103', firstname: 'MPHATSO',    othernames: 'JAMES',   surname: 'MWALE',       gender: 'Male',   dob: '22/July/1985',     expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00104', firstname: 'GRACE',      othernames: '',        surname: 'TEMBO',       gender: 'Female', dob: '17/November/1995', expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00105', firstname: 'INNOCENT',   othernames: '',        surname: 'CHIRWA',      gender: 'Male',   dob: '03/February/1991', expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00106', firstname: 'ESTHER',     othernames: 'RUTH',    surname: 'KAMANGA',     gender: 'Female', dob: '28/September/1987', expiry: '01/January/2080', status: 'VALID' },
+    { idno: 'VLD00107', firstname: 'DALITSO',    othernames: '',        surname: 'NKHOMA',      gender: 'Male',   dob: '14/April/1994',    expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00108', firstname: 'TAKONDWA',   othernames: '',        surname: 'GONDWE',      gender: 'Female', dob: '09/January/1996',  expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00109', firstname: 'FELIX',      othernames: 'PAUL',    surname: 'MSISKA',      gender: 'Male',   dob: '25/June/1983',     expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00110', firstname: 'STELLA',     othernames: '',        surname: 'NYIRENDA',    gender: 'Female', dob: '30/October/1990',  expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00111', firstname: 'AUBREY',     othernames: '',        surname: 'JERE',        gender: 'Male',   dob: '07/August/1992',   expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00112', firstname: 'PATRICIA',   othernames: 'ANN',     surname: 'KAUNDA',      gender: 'Female', dob: '19/December/1986', expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00113', firstname: 'WYCLIFFE',   othernames: '',        surname: 'CHAVULA',     gender: 'Male',   dob: '11/February/1997', expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00114', firstname: 'ANNIE',      othernames: '',        surname: 'MKANDAWIRE',  gender: 'Female', dob: '26/April/1994',    expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00115', firstname: 'OLIVER',     othernames: 'JOHN',    surname: 'MUNTHALI',    gender: 'Male',   dob: '02/September/1984', expiry: '01/January/2080', status: 'VALID' },
+    { idno: 'VLD00116', firstname: 'BERNADETTE', othernames: '',        surname: 'ZUZE',        gender: 'Female', dob: '16/July/1993',     expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00117', firstname: 'HASTINGS',   othernames: '',        surname: 'KAPITO',      gender: 'Male',   dob: '23/March/1989',    expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00118', firstname: 'MERCY',      othernames: 'HOPE',    surname: 'NGWIRA',      gender: 'Female', dob: '08/November/1991', expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00119', firstname: 'RAPHAEL',    othernames: '',        surname: 'KACHINGWE',   gender: 'Male',   dob: '15/January/1998',  expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00120', firstname: 'DIANA',      othernames: '',        surname: 'MALOPA',      gender: 'Female', dob: '31/May/1995',      expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00121', firstname: 'WINSTON',    othernames: 'CHARLES', surname: 'KADZAMIRA',   gender: 'Male',   dob: '19/October/1982',  expiry: '01/January/2080',  status: 'VALID' },
+    { idno: 'VLD00122', firstname: 'NATASHA',    othernames: '',        surname: 'THOLE',       gender: 'Female', dob: '04/August/1997',   expiry: '01/January/2080',  status: 'VALID' },
   ];
 
   constructor() {
@@ -140,5 +164,9 @@ export class App {
     const month = String(d.getMonth() + 1).padStart(2, '0');
     const day = String(d.getDate()).padStart(2, '0');
     return `${year}-${month}-${day}`;
+  }
+
+  copyId(idno: string) {
+    navigator.clipboard.writeText(idno);
   }
 }
